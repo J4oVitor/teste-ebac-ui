f71bd40866a33082980bf785e8a953254cf09fd6
Joao Vitor <jaovitosr902@gmail.com>
2025-04-28 13:52:03 -0300

    Objetos de Pagina

--- cypress/e2e/3-loja-ebac/Produtos.cy.js
+++ cypress/e2e/3-loja-ebac/Produtos.cy.js
@@ -1,18 +1,29 @@
 /// <reference types='cypress'/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .product-block')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
+        produtosPage.buscarProdutoLista('Aether Gym Pant')
             cy.get('#tab-title-additional_information > a').should('contain' , 'Informação adicional')
+    });
+    
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Pierce Gym Short'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+    
+    it('Deve visitar a pagina do produto', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
